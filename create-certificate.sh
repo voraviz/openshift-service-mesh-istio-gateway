2bcf818415e6c859550ec2abcd15caf41fa0096a
voravitl <voravitl@gmail.com>
2021-05-16 23:37:48 +0700

    create cert and private key

--- create-certificate.sh
+++ create-certificate.sh
@@ -8,3 +8,5 @@ openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=example In
 echo "Create Certificate and Private Key for $CN"
 openssl req -out certs/frontend.csr -newkey rsa:2048 -nodes -keyout certs/frontend.key -subj "/CN=${CN}/O=Great Department"
 openssl x509 -req -days 365 -CA certs/example.com.crt -CAkey certs/example.com.key -set_serial 0 -in certs/frontend.csr -out certs/frontend.crt
+cp certs/frontend.key certs/tls.key
+cp certs/frontend.crt certs/tls.crt
